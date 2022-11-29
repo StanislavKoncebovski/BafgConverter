d1f0bc1b3e23e4806b260e3c61cbd1168cc1e4c9
Stanislav <stanislav@pikkatech.eu>
2022-11-29 13:28:42 +0100

    Data

--- Code/bafg_converter.py
+++ Code/bafg_converter.py
@@ -6,6 +6,7 @@ from typing import Optional
 import numpy as np
 import pandas as pd
 import xml.etree.ElementTree as ET
+import glob
 
 from gauge import Gauge
 
@@ -21,15 +22,33 @@ class BafgConverter:
     #endregion
 
     def __init__(self):
-        self._data = []
+        self._data = {}
         self._gauges = None
-
 
     @property
     def gauges(self):
         return self._gauges
 
+    @property
+    def data(self):
+        return self._data
+
     #region Public Features
+    def create_gauge_list(self, file_name: str):
+        with open(file_name) as file:
+            doc = json.load(file)
+
+        gauges = []
+
+        for feature in doc['features']:
+            attributes = feature['attributes']
+            grid_number = int(attributes['grdc_no'])
+            river = attributes['river'].title()
+            station = attributes['station'].title()
+            gauges.append((grid_number, river, station))
+
+        self._gauges = pd.DataFrame(gauges, columns=['GridNo', 'River', 'Station'])
+
     def create_gauge_dataframe(self, file_name: str) -> Optional[pd.DataFrame]:
         '''
         Creates a pandas series from a BFG WML file
@@ -50,7 +69,7 @@ class BafgConverter:
         node_result = node_observation.find("om:result", BafgConverter.NAMESPACES)
         node_time_series = node_result.find("wml2:MeasurementTimeseries", BafgConverter.NAMESPACES)
 
-        self._data = []
+        data = []
 
         for node_point in node_time_series.findall("wml2:point", BafgConverter.NAMESPACES):
             node_measurement = node_point.find("wml2:MeasurementTVP", BafgConverter.NAMESPACES)
@@ -60,34 +79,35 @@ class BafgConverter:
             time = datetime.fromisoformat(node_time.text[:10])
             value = float(node_value.text) if node_value.text is not None else np.NaN
 
-            self._data.append((time.year, time.month, value))
+            data.append((time.year, time.month, value))
 
-        return pd.DataFrame(self._data, columns=['Year', 'Month', 'Discharge'])
+        df = pd.DataFrame(data, columns=['Year', 'Month', 'Discharge'])
+        self._data[gauge_id] = df
+        return df
 
-    def create_gauge_list(self, file_name: str):
-        with open(file_name) as file:
-            doc = json.load(file)
+    def create_gauge_dataframes(self, flie_names: list[str]):
+        for file_name in flie_names:
+            print(file_name)
+            self.create_gauge_dataframe(file_name)
 
-        gauges = []
-
-        for feature in doc['features']:
-            attributes = feature['attributes']
-            gauge = Gauge(grid_number=int(attributes['grdc_no']), river=attributes['river'].title(), station=attributes['station'].title())
-            gauges.append(gauge)
-
-        self._gauges = pd.DataFrame(gauges, columns=['GridNo', 'River', 'Station'])
+    def create_gauge_dataframes_from_folder(self, folder: str):
+        files = glob.glob(f"{folder}\\*.wml")
+        self.create_gauge_dataframes(files)
 
     #endregion
 
 if __name__ == '__main__':
     converter = BafgConverter()
-    # file_name = r"C:\Users\Stan\Desktop\-999_2316200_Q_Merge.Months.wml"
-    #
-    # discharges = converter.create_gauge_dataframe(file_name)
-    #
-    # print(discharges)
 
-    gauges_file_name = r"C:\Users\Stan\Desktop\stationbasins.geojson"
+    # create gauge list
+    gauges_file_name = r"../Data/stationbasins.geojson"
     converter.create_gauge_list(gauges_file_name)
-
     print(converter.gauges)
+
+    # create station dataframes
+    folder = "../Data/"
+    converter.create_gauge_dataframes_from_folder(folder)
+
+    # station_file_name = r"C:\Users\Stan\Desktop\-999_2316200_Q_Merge.Months.wml"
+    # converter.create_gauge_dataframe(station_file_name)
+    # print(converter.data)
